2523142c6018567e8b869428a087b988885e160d
Nikolay Donets <nd.startup@gmail.com>
2016-06-08 14:04:35 +0300

    add tests for the KeyboardButton

---
+++ src/test/scala/com/github/nikdon/telepooz/model/KeyboardButtonTest.scala
@@ -0,0 +1,38 @@
+package com.github.nikdon.telepooz.model
+
+import com.github.nikdon.telepooz.raw.{CirceDecoders, CirceEncoders}
+import com.github.nikdon.telepooz.tags
+import io.circe.syntax._
+import org.scalacheck.Arbitrary._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
+
+
+
+class KeyboardButtonTest extends FlatSpec
+                             with Matchers
+                             with GeneratorDrivenPropertyChecks
+                             with tags.Syntax
+                             with CirceEncoders
+                             with CirceDecoders {
+
+  behavior of "KeyboardButton"
+
+  import KeyboardButtonTest._
+
+  it should "convert to a json and back to a model" in {
+
+    forAll(keyboardButtonGen) { keyboardButton ⇒
+      val json = keyboardButton.asJson.noSpaces
+      io.circe.parser.decode[KeyboardButton](json) foreach (res ⇒ res shouldEqual keyboardButton)
+    }
+  }
+}
+
+object KeyboardButtonTest {
+  val keyboardButtonGen = for {
+    text ← arbitrary[String]
+    requestContact ← arbitrary[Option[Boolean]]
+    requestLocation ← arbitrary[Option[Boolean]]
+  } yield KeyboardButton(text, requestContact, requestLocation)
+}
