82503b7b66ca95dca8d1064a9ee31db02891fa5f
Nikolay Donets <nd.startup@gmail.com>
2016-06-08 14:09:05 +0300

    add tests for the InlineKeyboardButton

---
+++ src/test/scala/com/github/nikdon/telepooz/model/InlineKeyboardButtonTest.scala
@@ -0,0 +1,38 @@
+package com.github.nikdon.telepooz.model
+
+
+import com.github.nikdon.telepooz.raw.{CirceDecoders, CirceEncoders}
+import com.github.nikdon.telepooz.tags
+import io.circe.syntax._
+import org.scalacheck.Arbitrary._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
+
+
+class InlineKeyboardButtonTest extends FlatSpec
+                                       with Matchers
+                                       with GeneratorDrivenPropertyChecks
+                                       with tags.Syntax
+                                       with CirceEncoders
+                                       with CirceDecoders {
+  behavior of "InlineKeyboardButton"
+
+  import InlineKeyboardButtonTest._
+
+  it should "convert to a json and back to a model" in {
+
+    forAll(inlineKeyboardButtonGen) { inlineKeyboardButton ⇒
+      val json = inlineKeyboardButton.asJson.noSpaces
+      io.circe.parser.decode[InlineKeyboardButton](json) foreach (res ⇒ res shouldEqual inlineKeyboardButton)
+    }
+  }
+}
+
+object InlineKeyboardButtonTest extends tags.Syntax {
+  val inlineKeyboardButtonGen = for {
+    text ← arbitrary[String]
+    url ← arbitrary[Option[String]]
+    callbackData ← arbitrary[Option[String]]
+    switchInlineQuery ← arbitrary[Option[String]]
+  } yield InlineKeyboardButton(text, url, callbackData, switchInlineQuery)
+}
