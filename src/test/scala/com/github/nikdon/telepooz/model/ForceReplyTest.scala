f3df10de5cd86d842745bb3b0316c34e9dee1be0
Nikolay Donets <nd.startup@gmail.com>
2016-06-08 16:13:52 +0300

    add tests for the ForceReply

---
+++ src/test/scala/com/github/nikdon/telepooz/model/ForceReplyTest.scala
@@ -0,0 +1,36 @@
+package com.github.nikdon.telepooz.model
+
+
+import com.github.nikdon.telepooz.raw.{CirceDecoders, CirceEncoders}
+import com.github.nikdon.telepooz.tags
+import io.circe.syntax._
+import org.scalacheck.Arbitrary._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
+
+
+class ForceReplyTest extends FlatSpec
+                             with Matchers
+                             with GeneratorDrivenPropertyChecks
+                             with tags.Syntax
+                             with CirceEncoders
+                             with CirceDecoders {
+
+  behavior of "UserProfilePhotos"
+
+  import ForceReplyTest._
+
+  it should "convert to a json and back to a model" in {
+    forAll(forceReplyGen) { forceReply ⇒
+      val json = forceReply.asJson.noSpaces
+      io.circe.parser.decode[ForceReply](json) foreach (res ⇒ res shouldEqual forceReply)
+    }
+  }
+}
+
+object ForceReplyTest extends tags.Syntax {
+  val forceReplyGen = for {
+    forceReply ← arbitrary[Boolean]
+    selective ← arbitrary[Option[Boolean]]
+  } yield ForceReply(forceReply, selective)
+}
