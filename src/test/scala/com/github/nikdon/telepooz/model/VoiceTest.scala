a9f60fa39050b26912187e908d804b22c82c77ea
Nikolay Donets <nd.startup@gmail.com>
2016-06-08 14:20:44 +0300

    add tests for the Voice

--- src/test/scala/com/github/nikdon/telepooz/model/VoiceTest.scala
+++ src/test/scala/com/github/nikdon/telepooz/model/VoiceTest.scala
@@ -1,6 +1,5 @@
 package com.github.nikdon.telepooz.model
 
-
 import java.time.Duration
 
 import com.github.nikdon.telepooz.raw.{CirceDecoders, CirceEncoders}
@@ -11,34 +10,30 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
 
 
-class VideoTest extends FlatSpec
+class VoiceTest extends FlatSpec
                         with Matchers
                         with GeneratorDrivenPropertyChecks
                         with tags.Syntax
                         with CirceEncoders
                         with CirceDecoders {
+  behavior of "Voice"
 
-  behavior of "Video"
-
-  import VideoTest._
+  import VoiceTest._
 
   it should "convert to a json and back to a model" in {
 
-    forAll(videoGen) { video ⇒
-      val json = video.asJson.noSpaces
-      io.circe.parser.decode[Video](json) foreach (res ⇒ res shouldEqual video)
+    forAll(voiceGen) { voice ⇒
+      val json = voice.asJson.noSpaces
+      io.circe.parser.decode[Voice](json) foreach (res ⇒ res shouldEqual voice)
     }
   }
 }
 
-object VideoTest extends tags.Syntax {
-  val videoGen = for {
+object VoiceTest extends tags.Syntax {
+  val voiceGen = for {
     fileId ← arbitrary[String].map(_.fileId)
-    width ← arbitrary[Int]
-    height ← arbitrary[Int]
     duration ← arbitrary[Int].map(s ⇒ Duration.ofSeconds(s.toLong))
-    thumb ← arbitrary[Option[Boolean]].map(_ ⇒ PhotoSizeTest.photoSizeGen.sample)
-    mimeType ← arbitrary[Option[String]]
-    fileSize ← arbitrary[Option[Int]]
-  } yield Video(fileId, width, height, duration, thumb, mimeType, fileSize)
+    mimeType ← arbitrary[String]
+    fileSize ← arbitrary[Int]
+  } yield Voice(fileId, duration, mimeType, fileSize)
 }
