ca55f1219ec495028d49963adc4f163e34c88c22
Nikolay Donets <nd.startup@gmail.com>
2016-06-08 13:40:52 +0300

    add tests for the PhotoSize

---
+++ src/test/scala/com/github/nikdon/telepooz/model/PhotoSizeTest.scala
@@ -0,0 +1,38 @@
+package com.github.nikdon.telepooz.model
+
+import com.github.nikdon.telepooz.raw.{CirceDecoders, CirceEncoders}
+import com.github.nikdon.telepooz.tags
+import io.circe.syntax._
+import org.scalacheck.Arbitrary._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
+
+
+class PhotoSizeTest extends FlatSpec
+                            with Matchers
+                            with GeneratorDrivenPropertyChecks
+                            with tags.Syntax
+                            with CirceEncoders
+                            with CirceDecoders {
+
+  behavior of "PhotoSize"
+
+  import PhotoSizeTest._
+
+  it should "convert to a json and back to a model" in {
+
+    forAll(photoSizeGen) { photoSize ⇒
+      val json = photoSize.asJson.noSpaces
+      io.circe.parser.decode[PhotoSize](json) foreach (res ⇒ res shouldEqual photoSize)
+    }
+  }
+}
+
+object PhotoSizeTest extends tags.Syntax {
+  val photoSizeGen = for {
+    fileId ← arbitrary[String].map(_.fileId)
+    width ← arbitrary[Int]
+    height ← arbitrary[Int]
+    fileSize ← arbitrary[Option[Int]]
+  } yield PhotoSize(fileId, width, height, fileSize)
+}
