4f5ca6f8d6579d53d9e6fd745879c6ed99fd808e
Nikolay Donets <nd.startup@gmail.com>
2016-06-12 02:34:52 +0300

    add test for the ApiRequestExecutor

--- src/test/scala/com/github/nikdon/telepooz/ExecutorSpec.scala
+++ src/test/scala/com/github/nikdon/telepooz/ExecutorSpec.scala
@@ -1,72 +1,46 @@
 package com.github.nikdon.telepooz
 
-import akka.Done
-import com.github.nikdon.telepooz.engine.{MockApiRequestExecutor, Telepooz}
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import com.github.nikdon.telepooz.ToRawRequest.syntax._
+import com.github.nikdon.telepooz.engine.MockApiRequestExecutor
 import com.github.nikdon.telepooz.raw.CirceEncoders
+import com.github.nikdon.telepooz.tags.syntax._
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.scalatest.{FlatSpec, Matchers}
-
-import scala.concurrent.Await
-import scala.concurrent.duration._
+import org.scalatest.{FlatSpec, Matchers, OptionValues}
+import cats.implicits._
+import com.github.nikdon.telepooz.model.Response
+import org.scalatest.concurrent.ScalaFutures
 
 
 class ExecutorSpec extends FlatSpec
+                           with ScalaFutures
+                           with OptionValues
                            with Matchers
                            with GeneratorDrivenPropertyChecks
                            with CirceEncoders {
-
-//  val i = new MockApiRequestExecutor {}
-//
-//  implicit val ec = i.system.dispatcher
-//
-//  implicit def MonadFuture(implicit ec: ExecutionContext): Monad[Future] = new Monad[Future] {
-//    override def flatMap[A, B](fa: Future[A])(f: (A) ⇒ Future[B]): Future[B] = fa.flatMap(f)
-//    override def pure[A](x: A): Future[A] = Future.successful(x)
-//  }
-
   behavior of "Executor"
 
-//  it should "return a result of exe" in {
-//
-//    val res0 = (for {
-//      a ← api.execute(model.methods.GetMe.toRawRequest)
-//      b ← api.execute(model.methods.SendMessage(123.chatId, a.result.fold("empty")(_.first_name)).toRawRequest)
-//    } yield b).foldMap(i)
-//
-//    println("res0 from Future = " + Await.result(res0, Duration.Inf))
-//
-//    val res1 = (for {
-//      a ← api.execute(model.methods.SendMessage(123.chatId, "TEASDF").toRawRequest)
-//      b ← api.execute(model.methods.GetMe.toRawRequest)
-//    } yield b).foldMap(i)
-//
-//    println("res1 from Future = " + Await.result(res1, Duration.Inf))
-//
-//    val res2 = (for {
-//      c ← api.execute(ForwardMessage(123.chatId, "asdf".chatId, 321.messageId).toRawRequest)
-//      a ← api.execute(model.methods.SendMessage(123.chatId, "TEASDF").toRawRequest)
-//      b ← api.execute(model.methods.GetMe.toRawRequest)
-//    } yield b).foldMap(i)
-//
-//    println("res2 from Future = " + Await.result(res2, Duration.Inf))
-//
-//    val res3 = (for {
-//      d ← api.execute(GetUpdates().toRawRequest)
-//    } yield d).foldMap(i)
-//
-//    println("res3 from Future = " + Await.result(res3, Duration.Inf))
-//  }
+  implicit val system = ActorSystem("ExecutorSpecActorSys")
+  implicit val executor = system.dispatcher
+  implicit val materializer = ActorMaterializer()
+  val mockApiReqExe = new MockApiRequestExecutor
 
-  it should "poll" in {
-    val telebot = new Telepooz with MockApiRequestExecutor {
-      override def reactions: Map[String, Reaction] = Map(
-        "/test" → (implicit message ⇒ args ⇒ {
-          reply("Hi there!").map(_ ⇒ Done)
-        })
-      )
+  it should "allow to compose a requests" in {
+    val req = for {
+      a ← api.execute(model.methods.GetMe.toRawRequest)
+      b ← api.execute(model.methods.SendMessage(123.chatId, a.result.fold("empty")(_.first_name)).toRawRequest)
+    } yield b
+
+    val res = req.foldMap(mockApiReqExe)
+
+    whenReady(res){ m ⇒
+      m shouldBe a [Response[_]]
+      m.ok shouldEqual true
+      m.result shouldBe defined
+      m.result.value shouldBe a [model.Message]
     }
-
-    val asdf = telebot.run()
-    Await.ready(asdf, 20.seconds)
   }
 }
+
+
