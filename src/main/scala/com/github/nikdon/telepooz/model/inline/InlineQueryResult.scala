1698d1947ac5ea07a4481f95500c38333c4f7109
Nikolay Donets <nd.startup@gmail.com>
2016-06-20 21:55:08 +0300

    add InlineQueryResultGif

--- src/main/scala/com/github/nikdon/telepooz/model/inline/InlineQueryResult.scala
+++ src/main/scala/com/github/nikdon/telepooz/model/inline/InlineQueryResult.scala
@@ -64,3 +64,31 @@ case class InlineQueryResultPhoto(`type`: String = "photo", // TODO Move inside?
                                   caption: Option[String] = None,
                                   reply_markup: Option[InlineKeyboardMarkup] = None,
                                   input_message_content: Option[InputMessageContent] = None) extends InlineQueryResult
+
+
+/**
+  * Represents a link to an animated GIF file. By default, this animated GIF file will be sent by the user with optional
+  * caption. Alternatively, you can use input_message_content to send a message with the specified content instead of
+  * the animation.
+  *
+  * @param `type`                 Type of the result, must be gif
+  * @param id                     Unique identifier for this result, 1-64 bytes
+  * @param gif_url                A valid URL for the GIF file. File size must not exceed 1MB
+  * @param gif_width              Width of the GIF
+  * @param gif_height             Height of the GIF
+  * @param thumb_url              URL of the static thumbnail for the result (jpeg or gif)
+  * @param title                  Title for the result
+  * @param caption                Caption of the GIF file to be sent, 0-200 characters
+  * @param reply_markup           Inline keyboard attached to the message
+  * @param input_message_content  Content of the message to be sent instead of the GIF animation
+  */
+case class InlineQueryResultGif(`type`: String = "gif", // TODO Move inside?
+                                id: String @@ ResultId,
+                                gif_url: String,
+                                gif_width: Option[Int] = None,
+                                gif_height: Option[Int] = None,
+                                thumb_url: String,
+                                title: Option[String] = None,
+                                caption: Option[String] = None,
+                                reply_markup: Option[InlineKeyboardMarkup] = None,
+                                input_message_content: Option[InputMessageContent] = None) extends InlineQueryResult
