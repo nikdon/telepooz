7c19bbd822173dfe37f8c710346f8b7a5c362b25
tjugo <nikdon@users.noreply.github.com>
2016-10-25 11:38:40 +0300

    fix scaladoc err in the File.scala

--- src/main/scala/com/github/nikdon/telepooz/model/File.scala
+++ src/main/scala/com/github/nikdon/telepooz/model/File.scala
@@ -7,7 +7,7 @@ import shapeless.tag.@@
 /**
   * This object represents a file ready to be downloaded. The file can be downloaded via the link
   * [[https://api.telegram.org/file/bot<token>/<file_path>]]. It is guaranteed that the link will be
-  * valid for at least 1 hour. When the link expires, a new one can be requested by calling [[GetFile]].
+  * valid for at least 1 hour. When the link expires, a new one can be requested by calling `GetFile`.
   *
   * @param file_id   Unique identifier for this file
   * @param file_size File size, if known
