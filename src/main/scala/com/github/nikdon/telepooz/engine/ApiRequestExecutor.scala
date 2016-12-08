41c3b2f9ee338757824117f99396986dc29ad51c
Nikolay Donets <nd.startup@gmail.com>
2016-12-09 00:34:19 +0300

    refactor dropNulls

--- src/main/scala/com/github/nikdon/telepooz/engine/ApiRequestExecutor.scala
+++ src/main/scala/com/github/nikdon/telepooz/engine/ApiRequestExecutor.scala
@@ -28,13 +28,13 @@ import com.github.nikdon.telepooz.raw.RawRequest._
 import com.github.nikdon.telepooz.raw._
 import com.typesafe.config.{Config, ConfigFactory}
 import de.heikoseeberger.akkahttpcirce.CirceSupport
-import io.circe.syntax._
-import io.circe.{Decoder, Json, JsonObject}
+import io.circe.{Decoder, Json, Printer}
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
-
-abstract class ApiRequestExecutor(implicit system: ActorSystem, materializer: Materializer, ec: ExecutionContextExecutor)
+abstract class ApiRequestExecutor(implicit system: ActorSystem,
+                                  materializer: Materializer,
+                                  ec: ExecutionContextExecutor)
     extends (RawRequest ~> Future)
     with CirceSupport
     with CirceEncoders
@@ -46,51 +46,48 @@ abstract class ApiRequestExecutor(implicit system: ActorSystem, materializer: Ma
 
   private[this] lazy val http = Http()
 
-  private[this] def dropNulls(j: Json): Json = j.withObject { c ⇒
-    val fields = c.toList.filterNot { case (f, v) ⇒ v.isNull }
-    JsonObject.fromIterable(fields).asJson
-  }
+  private[this] val dropNulls = Printer.noSpaces.copy(dropNullKeys = true)
 
   private[this] def go[B: Decoder](methodName: String, payload: Json): Future[Response[B]] = {
     val uri = "https://" |+| telegramHost |+| "/bot" + token |+| "/" + methodName
     for {
-      response ← http.singleRequest(RequestBuilding.Post(Uri(uri), content = dropNulls(payload)))
-      decoded  ← circeUnmarshaller(responseDecoder).apply(response.entity)
+      response <- http.singleRequest(RequestBuilding.Post(Uri(uri), content = payload.pretty(dropNulls)))
+      decoded  <- circeUnmarshaller(responseDecoder).apply(response.entity)
     } yield decoded
   }
 
   override def apply[A](fa: RawRequest[A]): Future[A] = fa match {
-    case m: GetMe.type             ⇒ go(m.name, m.payload)
-    case m: GetWebhookInfo.type    ⇒ go(m.name, m.payload)
-    case m: SendMessage            ⇒ go(m.name, m.payload)
-    case m: SendGame               ⇒ go(m.name, m.payload)
-    case m: SetGameScore           ⇒ go(m.name, m.payload)
-    case m: GetGameHighScores      ⇒ go(m.name, m.payload)
-    case m: ForwardMessage         ⇒ go(m.name, m.payload)
-    case m: GetUpdates             ⇒ go(m.name, m.payload)
-    case m: SendPhoto              ⇒ go(m.name, m.payload)
-    case m: SendAudio              ⇒ go(m.name, m.payload)
-    case m: SendDocument           ⇒ go(m.name, m.payload)
-    case m: SendSticker            ⇒ go(m.name, m.payload)
-    case m: SendVideo              ⇒ go(m.name, m.payload)
-    case m: SendVoice              ⇒ go(m.name, m.payload)
-    case m: SendLocation           ⇒ go(m.name, m.payload)
-    case m: SendVenue              ⇒ go(m.name, m.payload)
-    case m: SendContact            ⇒ go(m.name, m.payload)
-    case m: SendChatAction         ⇒ go(m.name, m.payload)
-    case m: GetUserProfilePhotos   ⇒ go(m.name, m.payload)
-    case m: GetFile                ⇒ go(m.name, m.payload)
-    case m: KickChatMember         ⇒ go(m.name, m.payload)
-    case m: LeaveChat              ⇒ go(m.name, m.payload)
-    case m: UnbanChatMember        ⇒ go(m.name, m.payload)
-    case m: GetChat                ⇒ go(m.name, m.payload)
-    case m: GetChatAdministrators  ⇒ go(m.name, m.payload)
-    case m: GetChatMembersCount    ⇒ go(m.name, m.payload)
-    case m: GetChatMember          ⇒ go(m.name, m.payload)
-    case m: AnswerCallbackQuery    ⇒ go(m.name, m.payload)
-    case m: EditMessageCaption     ⇒ go(m.name, m.payload)
-    case m: EditMessageReplyMarkup ⇒ go(m.name, m.payload)
-    case m: EditMessageText        ⇒ go(m.name, m.payload)
-    case m: SetWebhook             ⇒ go(m.name, m.payload)
+    case m: GetMe.type             => go(m.name, m.payload)
+    case m: GetWebhookInfo.type    => go(m.name, m.payload)
+    case m: SendMessage            => go(m.name, m.payload)
+    case m: SendGame               => go(m.name, m.payload)
+    case m: SetGameScore           => go(m.name, m.payload)
+    case m: GetGameHighScores      => go(m.name, m.payload)
+    case m: ForwardMessage         => go(m.name, m.payload)
+    case m: GetUpdates             => go(m.name, m.payload)
+    case m: SendPhoto              => go(m.name, m.payload)
+    case m: SendAudio              => go(m.name, m.payload)
+    case m: SendDocument           => go(m.name, m.payload)
+    case m: SendSticker            => go(m.name, m.payload)
+    case m: SendVideo              => go(m.name, m.payload)
+    case m: SendVoice              => go(m.name, m.payload)
+    case m: SendLocation           => go(m.name, m.payload)
+    case m: SendVenue              => go(m.name, m.payload)
+    case m: SendContact            => go(m.name, m.payload)
+    case m: SendChatAction         => go(m.name, m.payload)
+    case m: GetUserProfilePhotos   => go(m.name, m.payload)
+    case m: GetFile                => go(m.name, m.payload)
+    case m: KickChatMember         => go(m.name, m.payload)
+    case m: LeaveChat              => go(m.name, m.payload)
+    case m: UnbanChatMember        => go(m.name, m.payload)
+    case m: GetChat                => go(m.name, m.payload)
+    case m: GetChatAdministrators  => go(m.name, m.payload)
+    case m: GetChatMembersCount    => go(m.name, m.payload)
+    case m: GetChatMember          => go(m.name, m.payload)
+    case m: AnswerCallbackQuery    => go(m.name, m.payload)
+    case m: EditMessageCaption     => go(m.name, m.payload)
+    case m: EditMessageReplyMarkup => go(m.name, m.payload)
+    case m: EditMessageText        => go(m.name, m.payload)
+    case m: SetWebhook             => go(m.name, m.payload)
   }
 }
